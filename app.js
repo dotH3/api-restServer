38da3fda766e0490f53c3058cdfc9a06643fafa4
h3 <joaquinbastias321@gmail.com>
2023-01-17 01:54:53 -0300

    17/01/23

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 var cors = require('cors');
 const dbConnection = require('./db/config');
+const { request, response } = require('express');
 
 const app = express();
 const port = process.env.PORT;
@@ -12,16 +13,21 @@ const paths = {
     users: '/api/users',
     auth: "/api/auth",
     products: "/api/products",
+    prisma: "/api/prisma"
 }
 
-dbConnection();
+// dbConnection();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use(paths.auth, require('./routes/auth'));
-app.use(paths.users, require('./routes/users'));
-app.use(paths.products, require('./routes/products'));
+// app.use(paths.auth, require('./routes/auth'));
+// app.use(paths.users, require('./routes/users'));
+// app.use(paths.products, require('./routes/products'));
+
+app.use(paths.prisma,async(req=request,res=response)=>{
+    res.send('asd')
+});
 
 app.listen(port, () => {
     console.clear();
